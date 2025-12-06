0729747d9b39043f9c35784af835572cdf3d4b6f
oussama Essalmani <essalmanioussama@gmail.com>
2025-12-06 23:20:13 +0100

    fix error of invantory service and billing service

--- inventary-service/src/main/java/ma/emsi/inventaryservice/InventaryServiceApplication.java
+++ inventary-service/src/main/java/ma/emsi/inventaryservice/InventaryServiceApplication.java
@@ -1,7 +1,15 @@
 package ma.emsi.inventaryservice;
 
+import ma.emsi.inventaryservice.entities.Product;
+import ma.emsi.inventaryservice.repositories.ProductRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.UUID;
 
 @SpringBootApplication
 public class InventaryServiceApplication {
@@ -10,4 +18,43 @@ public class InventaryServiceApplication {
 		SpringApplication.run(InventaryServiceApplication.class, args);
 	}
 
+
+
+	@Bean
+	CommandLineRunner init(ProductRepository productRepository) {
+		return args -> {
+			productRepository.save(Product.builder()
+
+					.id(UUID.randomUUID().toString())
+					.name("computer")
+					.price(3200)
+					.quantity(11)
+					.build()
+
+
+			);
+			productRepository.save(Product.builder()
+
+					.id(UUID.randomUUID().toString())
+					.name("Printer")
+					.price(3200)
+					.quantity(11)
+					.build()
+
+
+			);
+			productRepository.save(Product.builder()
+
+					.id(UUID.randomUUID().toString())
+					.name("Smart phone")
+					.price(3200)
+					.quantity(11)
+					.build()
+
+
+			);
+
+
+		};
+	}
 }
---
+++ inventary-service/src/main/java/ma/emsi/inventaryservice/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package ma.emsi.inventaryservice.repositories;
+
+import ma.emsi.inventaryservice.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface ProductRepository extends JpaRepository<Product,String> {
+
+
+}
